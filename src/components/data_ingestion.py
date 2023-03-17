2a25a80710b5922b1bb0bb42bcf5277148f3dabe
mdhimran <mdhimran99@gmail.com>
2023-03-17 16:44:49 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,8 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation,DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainer,ModelTrainerConfig
+
 @dataclass # helps to directly define a class - best only when we have variables not with functions
 class DataIngestionConfig:
     train_data_path: str = os.path.join("artifacts","train.csv")
@@ -36,7 +38,7 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Ingestion of data is  completed")
+            logging.info("Ingestion of data is completed")
 
             return (
                 self.ingestion_config.train_data_path,
@@ -50,4 +52,7 @@ if __name__=="__main__":
     train_data,test_data = obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
